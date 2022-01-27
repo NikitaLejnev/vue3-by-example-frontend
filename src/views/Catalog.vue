f8ad958e75d847a98a0ade162e37e02395b94ad6
NikitaLejnev <catassasin331@gmail.com>
2022-01-27 09:09:19 +0300

    Added Catalog.vue

---
+++ src/views/Catalog.vue
@@ -0,0 +1,18 @@
+<template>
+  <Card v-for="c of catalog" :key="c.id">
+    <template #header>
+      <img :alt="c.description" :src="c.image_url" />
+    </template>
+    <template #title>{{ c.name }}</template>
+    <template #content>{{ c.description }}</template>
+    <template #footer>
+      <Button icon="pi pi-check" label="Book" class="p-button-secondary" @click="book(c.id)" />
+      <Dialog header="Create Booking" v-model:visible="displayBookingForm">
+        <BookingForm
+          @booking-form-close="displayBookingForm = false; displayMessage(); "
+          :selectedCatalogId="selectedCatalogId"
+        />
+      </Dialog>
+    </template>
+  </Card>
+</template>
