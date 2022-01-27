a07a720b87c451e813bbb1192d969bf5b8b981f4
NikitaLejnev <catassasin331@gmail.com>
2022-01-27 07:54:00 +0300

    Created BookingForm.vue and added name field

---
+++ src/components/BookingForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <Form @submit="onSubmit" :validation-schema="schema">
+    <Field v-slot="{ field, errors }" v-model="name" name="name">
+      <div class="p-col-12">
+        <div class="p-inputgroup">
+          <InputText placeholder="Name" :class="{ 'p-invalid': errors.length > 0 }" v-bind="field" />
+        </div>
+        <small class="p-error" v-if="errors.length > 0">Name is invalid</small>
+      </div>
+    </Field>
+  </Form>
+</template>
